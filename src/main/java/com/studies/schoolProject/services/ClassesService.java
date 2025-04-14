8cc57809e0e0e75e419efb553d4a11f64e20aba6
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-04-14 19:23:12 -0300

    Classes service created

---
+++ src/main/java/com/studies/schoolProject/services/ClassesService.java
@@ -0,0 +1,25 @@
+package com.studies.schoolProject.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studies.schoolProject.entities.Classes;
+import com.studies.schoolProject.repositories.ClassesRepository;
+
+@Service
+public class ClassesService {
+
+    @Autowired
+    private ClassesRepository classesRepository;
+
+    public List<Classes> findAll() {
+        return classesRepository.findAll();
+    }
+
+    public Classes findById(long id) {
+        var obj = classesRepository.findById(id);
+        return obj.get();
+    }
+}
