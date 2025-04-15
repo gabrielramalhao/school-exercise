098890c1bdf99ea1b49d02d222f4630cc521fb96
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-04-15 18:06:07 -0300

    Student service created

---
+++ src/main/java/com/studies/schoolProject/services/StudentService.java
@@ -0,0 +1,44 @@
+package com.studies.schoolProject.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.studies.schoolProject.entities.Student;
+import com.studies.schoolProject.repositories.StudentRepository;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    public List<Student> findAll() {
+        return studentRepository.findAll();
+    }
+
+    public Student findById(long id) {
+        var obj = studentRepository.findById(id);
+        return obj.get();
+    }
+
+    public void remove(long id) {
+        studentRepository.deleteById(id);
+    }
+
+    public void insert(Student student) {
+        studentRepository.save(student);
+    }
+
+    public Student update(Student obj, long id) {
+        var entity = studentRepository.getReferenceById(id);
+        updateData(entity, obj);
+        return studentRepository.save(entity);
+    }
+
+
+    private void updateData(Student entity, Student obj) {
+        entity.setName(obj.getName());
+    }
+}
