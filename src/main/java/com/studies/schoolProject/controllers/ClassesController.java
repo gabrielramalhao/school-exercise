99964fc0e4fd084e9cdf03bfeb362b244067816f
Gabriel Ramalhao <gabriel123.ramalhao@gmail.com>
2025-04-23 18:18:56 -0300

    ClassesController modified

--- src/main/java/com/studies/schoolProject/controllers/ClassesController.java
+++ src/main/java/com/studies/schoolProject/controllers/ClassesController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -35,7 +35,7 @@ public class ClassesController {
         return ResponseEntity.ok().body(obj);
     }
 
-    @PutMapping
+    @PostMapping
     public ResponseEntity<Classes> insert(@RequestBody Classes obj) {
         classesService.insert(obj);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
